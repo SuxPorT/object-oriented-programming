de49b648b2a1d9681915f87554feeda53d14fb15
SuxPorT <51892110+SuxPorT@users.noreply.github.com>
2019-10-02 12:40:47 -0300

    Update Teste.java

--- NetBeansProjects/Trabalho08/src/Sistema/Teste.java
+++ NetBeansProjects/Trabalho08/src/Sistema/Teste.java
@@ -8,13 +8,15 @@ import Exceptions.ExcecaoSenhaInvalida;
 public class Teste {
     public static void main(String[] args) {
         
+        Banco bradesco = new Banco("Bradesco");
+        
+        bradesco.criar_conta(1234, "abcd", 2000);
+        bradesco.criar_conta(1111, "qwer", 1500);
+        bradesco.criar_conta(1010, "test", 1000);
+        bradesco.criar_conta(2019, "2019", 5500);
+        bradesco.criar_conta(007, "bond", 1999);
+        
         try {
-            Banco bradesco = new Banco("Bradesco");
-            bradesco.criar_conta(1234, "abcd", 2000);
-            bradesco.criar_conta(1111, "qwer", 1500);
-            bradesco.criar_conta(1010, "test", 1000);
-            bradesco.criar_conta(2019, "2019", 5500);
-            bradesco.criar_conta(007, "bond", 1999);
 
             bradesco.sacar(1234, "abcd", 1900);
             bradesco.sacar(1111, "qwer", 1000);
@@ -25,7 +27,7 @@ public class Teste {
             bradesco.sacar(1234, "abcd", 100);
             
             bradesco.sacar(999, "qwer", 100);    // Numero = 1111    ExcecaoContaInvalida
-            bradesco.sacar(1234, "abcd", 100);    // Saldo = 0           ExcecaoSaldoInsuficiente
+            bradesco.sacar(1234, "abcd", 100);    // Saldo = 0       ExcecaoSaldoInsuficiente
             bradesco.sacar(007, "james", 500);    // Senha = bond    ExcecaoSenhaInvalida
             // ExcecaoHorarioInvalido com a variável da classe Banco hora < 8 ou hora > 22
         }
